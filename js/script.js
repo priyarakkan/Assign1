2f63054787c6efa21ffe44e8bc95966e07f289df
priyarakkan <priyadharsinikdp@gmail.com>
2023-02-14 17:05:05 -0800

    update step2 in js
    adding some literal expression

--- js/script.js
+++ js/script.js
@@ -4,3 +4,9 @@ function theMonth();
 let currentMonth;
 let summerMonth;
 let myLibraryFunction;
+
+// STEP 2
+6         // Numeric literal
+"Rashmi"  // String literal
+true      // Boolean literal
+null      // null literal value
